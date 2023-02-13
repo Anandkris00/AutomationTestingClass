c63c07b82f1154fde6fef3dcf5ec43fea534976c
Anand Krishna S <anand.krishna@experionglobal.local>
2023-02-13 15:08:09 +0530

    mesg

---
+++ SimpleCode/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10, temp;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
